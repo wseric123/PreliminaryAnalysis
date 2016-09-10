89713d8db1a8a28da2ac3ce375e23c611941255f
wseric123 <shen.wang@gatech.edu>
2016-09-10 16:03:04 -0400

    Update FPItoLPOCorr.R

--- FPItoLPOCorr.R
+++ FPItoLPOCorr.R
@@ -1,6 +1,7 @@
 library(arules)
 
 df= read.csv("F:/Documents/Senior design/Janssen Gatech/new file/Actual_FPI_to_LPO.csv", header=FALSE)
-dfs=str(df)
-vn=apriori(dfs)
+#Shen's edit: instead of assign the str funtion to a variable dfs, directly call the function
+str(df)
+vn=apriori(df)
 inspect(vn)
